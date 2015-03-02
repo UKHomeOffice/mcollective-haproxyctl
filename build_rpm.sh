00373c1c4b427eeff1e572f3fb0718ab0bad4d5b
Adam Scarborough <scarby@users.noreply.github.com>
2015-03-02 17:26:06 +0000

    increment build number

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fpm -s dir -t rpm -v 0.1.1 -n mcollective-agent-haproxyctl -a noarch --after-install postinst.sh --after-upgrade postinst.sh src/
+fpm -s dir -t rpm -v 0.1.2 -n mcollective-agent-haproxyctl -a noarch --after-install postinst.sh --after-upgrade postinst.sh src/
